21f91298e0b925d50c07a69a78df157f1ccbad6c
jongwon <54282927+loosie@users.noreply.github.com>
2022-04-19 00:00:06 +0900

    feat: redis 라이브러리 및 config 설정 추가

---
+++ src/main/java/com/techdot/techdot/infra/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.techdot.techdot.infra.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory(){
+		return new LettuceConnectionFactory();
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+		GenericJackson2JsonRedisSerializer genericJackson2JsonSerializer = new GenericJackson2JsonRedisSerializer();
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(genericJackson2JsonSerializer);
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashValueSerializer(genericJackson2JsonSerializer);
+		return redisTemplate;
+	}
+
+}
