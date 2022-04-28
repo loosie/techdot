632fbdfe6d3f6068c88a942c6581b728c549f90b
jongwon <54282927+loosie@users.noreply.github.com>
2022-04-29 00:11:00 +0900

    feat: s3 이미지 http 캐시 적용

--- src/main/java/com/techdot/techdot/modules/post/image/RealPostS3Service.java
+++ src/main/java/com/techdot/techdot/modules/post/image/RealPostS3Service.java
@@ -53,6 +53,22 @@ public class RealPostS3Service implements PostS3Service{
 		String fileStorageName = FOLDER_NAME + fileName;
 
 		ObjectMetadata metadata = new ObjectMetadata();
+
+		/**
+		 * HTTP 캐싱 Cache-Control
+		 * max-age: 브라우저에 적용되는 캐시 만료 기한.
+		 *   브라우저는 서버에 요청을 보내지 않고 디스크 또는 메모리에서만 캐시를 읽어와 계속 사용한다.
+		 *
+		 *   주의해야 할 점은 한 번 브라우저에 캐시가 저장되면 만료될 때까지 캐시는 계속 브라우저에 남아 있게 된다.
+		 *   때문에 CDN Invalidation을 포함한 서버의 어떤 작업이 있어도 브라우저의 유효한 캐시를 지우기는 어렵다.
+		 *
+		 * s-maxage: 중간 서버에서만 적용되는 max-age를 설정하는 값
+		 *    's-maxage=31536000, max-age=0'과 같이 설정하면 CDN에서는 1년동안 캐시되지만 브라우저에서는 매번 재검증 요청을 보내도록 설정할 수 있다.
+		 *    썸네일 이미지는 변경될 가능성이 있기 때문에 브라우저가 아닌 중간 서버 CDN까지만 캐싱하도록 설정한다.
+		 *
+		 * 참고: https://toss.tech/article/smart-web-service-cache
+		 */
+		metadata.setCacheControl("s-maxage=31536000, max-age=0");
 		metadata.setContentType(MediaType.IMAGE_PNG_VALUE);
 		metadata.setContentLength(file.getSize());
 
